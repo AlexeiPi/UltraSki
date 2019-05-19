8e091a9eac831c42413163b78067aceb79fa6c07
alp@microgate.ru <AlexeiPi>
2019-05-19 04:24:40 +0300

    nothing

--- RaceViews.cpp
+++ RaceViews.cpp
@@ -8,7 +8,7 @@
 #pragma package(smart_init)
 //---------------------------------------------------------------------------
 void __fastcall RaceStartListView::form_resize(TObject *Sender){
-	checkLines();   //new commit 4
+	checkLines();   //new commit 5
 }
 //______________________________________________________________________________
 void __fastcall RaceStartListView::checkLines(void){
